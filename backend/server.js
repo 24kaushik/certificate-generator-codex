74f47762dca232367c68a4813b56b3e8e0ce42fb
Kaushik <akaushikarkar@gmail.com>
2025-09-07 20:16:24 +0530

    test

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,6 @@
 import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
-import bodyParser from "body-parser";
 import connectDB from "./config/db.js";
 
 // Configure environment variables, Express setup, db.
